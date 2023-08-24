20c5b6ac9afeb832ab8112ba0fea318f25a92c23
ReyTroan <reytroan@gmail.com>
2023-08-24 19:30:25 +0300

    First Commit

---
+++ src/com/teachmeskills/lesson8/Figure.java
@@ -0,0 +1,71 @@
+package com.teachmeskills.lesson8;
+
+/**
+ * I wrote a hierarchy of "Figure" classes.
+ * Made the Shape class abstract, added abstract methods for calculating the area of the shape and perimeter.
+ */
+sealed abstract class Figure permits Triangle, Rectangle, Circle {
+    public abstract double getArea();
+    public abstract double getPerimeter();
+}
+
+final class Triangle extends Figure {
+    double a;
+    double b;
+    double c;
+
+    public Triangle(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    @Override
+    public double getArea() {
+        double x = (a + b + c) / 2;
+        return Math.sqrt(x * (x - a) * (x - b) * (x - c));
+    }
+
+    @Override
+    public double getPerimeter() {
+        return a + b + c;
+    }
+}
+
+final class Rectangle extends Figure {
+    double width;
+    double height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public double getArea() {
+        return width * height;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * (width + height);
+    }
+}
+
+final class Circle extends Figure {
+    double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+}
---
+++ src/com/teachmeskills/lesson8/Runner.java
@@ -0,0 +1,29 @@
+package com.teachmeskills.lesson8;
+
+/**
+ * I cycled through the array and for each figure from the array I displayed the area on the screen
+ * Displayed the sum of the perimeter of all the shapes in the array.
+ */
+public class Runner {
+    public static void main(String[] args) {
+        Figure[] arrayFigures = new Figure[5];
+        arrayFigures[0] = new Triangle(3.0, 3.0, 3.0);
+        arrayFigures[1] = new Rectangle(4.0, 8.0);
+        arrayFigures[2] = new Circle(2.0);
+        arrayFigures[3] = new Rectangle(9.0, 6.0);
+        arrayFigures[4] = new Triangle(6.0, 8.0, 10.0);
+
+        double totalPerimeter = 0.0;
+
+        for (Figure i : arrayFigures) {
+            double area = i.getArea();
+            System.out.println("The area of the figure: " + area);
+
+            double perimeter = i.getPerimeter();
+
+            totalPerimeter += perimeter;
+        }
+
+        System.out.println("Sum of the perimeters of all shapes: " + totalPerimeter);
+    }
+}
