ef305aa76c638b42c5457482e650232cda825cf5
ReyTroan <reytroan@gmail.com>
2023-08-26 22:25:18 +0300

    remaking the for loop into a foreach loop on line 41

--- src/com/teachmeskills/lesson8/part2/Matrix.java
+++ src/com/teachmeskills/lesson8/part2/Matrix.java
@@ -38,9 +38,9 @@ public class Matrix {
 
     static class Utils{
         public static void matrixOutput(int[][] matrix){
-            for (int i = 0; i < matrix.length; i++){
-                for (int j = 0; j < matrix[i].length; j++){
-                    System.out.print(matrix[i][j] + " ");
+            for (int[] string : matrix) {
+                for (int element : string) {
+                    System.out.print(element + " ");
                 }
             }
             System.out.println();
